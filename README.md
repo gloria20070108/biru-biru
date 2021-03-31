5a91f769a80bf421d23ba36059cb8d6c83993c95
Zhangzhang Li <gloria20070108@hotmail.com>
2021-03-30 23:26:11 -0700

    init commit for react and other setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # biru-biru
+
+Project 3 of [CS5610](https://johnguerra.co/classes/webDevelopment_spring_2021/)
+
+## Author
+
+Na Lou, Zhangzhang Li
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <main>Hello Biru Biru!</main>
+
+      <footer>By Zhangzhang and Na</footer>
+    </div>
+  );
+}
+
+App.propTypes = {};
