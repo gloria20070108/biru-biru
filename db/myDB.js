ecd4b8143520b95cac81c8c061340c2bf2f49a21
nlou9 <nlou82@gmail.com>
2021-04-04 00:16:43 -0700

    signup function

---
+++ db/myDB.js
@@ -0,0 +1,73 @@
+const MongoClient = require("mongodb").MongoClient;
+const dotenv = require("dotenv");
+const q = require("q");
+
+dotenv.config();
+const uri = process.env.MONGO_URI;
+
+exports.localReg = (username, password) => {
+  const deferred = q.defer();
+
+  MongoClient.connect(uri, (err, client) => {
+    const db = client.db("users");
+    const collection = db.collection("localUsers");
+
+    //check if username is already assigned in our database
+    collection.findOne({ username: username }).then((result) => {
+      if (null != result) {
+        console.log("USERNAME ALREADY EXISTS:", result.username);
+        deferred.resolve(false); // username exists
+      } else {
+        const user = {
+          username: username,
+          password: password,
+        };
+
+        console.log("CREATING USER:", username);
+
+        collection.insertOne(user).then(() => {
+          client.close();
+          deferred.resolve(user);
+        });
+      }
+    });
+  });
+
+  return deferred.promise;
+};
+
+exports.localAuth = (username, password) => {
+  const deferred = q.defer();
+
+  MongoClient.connect(uri, (err, client) => {
+    const db = client.db("users");
+    const collection = db.collection("localUsers");
+
+    collection.findOne({ username: username }).then((result) => {
+      if (null == result) {
+        console.log("USER NOT FOUND:", username);
+
+        deferred.resolve(false);
+      } else {
+        console.log("FOUND USER: " + result.username);
+
+        if (result.password === password) {
+          deferred.resolve(result);
+        } else {
+          console.log("AUTHENTICATION FAILED");
+          deferred.resolve(false);
+        }
+      }
+
+      client.close();
+    });
+  });
+
+  return deferred.promise;
+};
+
+
+
+
+
+
--- front/src/Pages/SignupPage.js
+++ front/src/Pages/SignupPage.js
@@ -1,8 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function SignupPage(){
-  return(
-    <div className="app">
-      <h1> Signup Page</h1>
-    </div>);
+export default function SignupPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function registerUsers() {
+    console.log("calling send data function");
+    console.log("username", { username });
+    console.log("password", { password });
+    const response = await fetch("./register", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    if (response.status !== 200) {
+      console.log("Can't sign up current user. Please try again.");
+    } else {
+      window.location.href = "/home";
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("calling submit function");
+    registerUsers();
+  };
+
+  return (
+    <div>
+      <form id="sign-up-form" onSubmit={handleSubmit}>
+        <label htmlFor="username">User Name</label>
+        <input
+          type="text"
+          id="sign-up-username"
+          name="username"
+          value={username}
+          className="form-control text-content"
+          placeholder="New Username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          id="sign-up-password"
+          value={password}
+          name="password"
+          className="form-control text-content"
+          placeholder="New Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button
+          className="w-100 btn btn-lg btn-success sign-up-btn"
+          type="submit"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
 }
