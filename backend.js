cbf646b54f01a7efdde0f759e5c0fd078d0f0cbd
nlou9 <nlou82@gmail.com>
2021-04-04 11:44:23 -0700

    change index and backend

--- backend.js
+++ backend.js
@@ -1,18 +1,20 @@
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var passport = require("passport");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const app = express();
+var indexRouter = require('./routes/index');
+var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "front/build")));
+app.use(express.static(path.join(__dirname, 'front/build')));
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
+app.use(passport.initialize());
+app.use(passport.session());
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,61 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
+var LocalStrategy = require("passport-local").Strategy;
+const MyDB = require("../db/MyDB");
+const passport = require("passport");
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-  res.render("index", { title: "Express" });
+
+router.post("/register", (req, res, next) => {
+  console.log("backend signup functionc calling");
+  passport.authenticate("local-signup", (error, user) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+
+    req.login(user, (error) => {
+      if (error) {
+        return res.status(500).json(error);
+      } else {
+        return res.json({
+          message: "successfully sign up and sign in!",
+        });
+      }
+    });
+  })(req, res, next);
 });
 
+passport.use(
+  "local-signup",
+  new LocalStrategy(
+    { passReqToCallback: true },
+    (req, username, password, done) => {
+      MyDB.localReg(username, password)
+        .then((user) => {
+          if (user) {
+            console.log("REGISTERED: " + user.username);
+            req.session.success =
+              "You are successfully registered and logged in " +
+              user.username +
+              "!";
+            done(null, user);
+          } else {
+            console.log("COULD NOT REGISTER");
+            req.session.error =
+              "That username is already in use, please try a different one."; //inform user could not log them in
+            done(null, user);
+          }
+        })
+        .fail((err) => {
+          console.log(err.body);
+        });
+    }
+  )
+);
+passport.serializeUser(function (user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser((username, done) => {
+  done(null, username);
+});
 module.exports = router;
