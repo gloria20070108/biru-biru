cb82ae2439f7aa363c4d907ff76f8f72aaacb10c
nlou9 <nlou82@gmail.com>
2021-04-09 23:36:09 -0700

    change backend with auth

--- backend.js
+++ backend.js
@@ -1,20 +1,20 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var passport = require("passport");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const passport = require("passport");
 
-var indexRouter = require("./routes/index");
-var app = express();
+const indexRouter = require("./routes/index");
+const myAuth = require("./auth/MyAuth");
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+myAuth.setupPassport(app);
 app.use(express.static(path.join(__dirname, "front/build")));
-
+app.use("/", myAuth.authRouter());
 app.use("/", indexRouter);
-app.use(passport.initialize());
-app.use(passport.session());
 
 module.exports = app;
